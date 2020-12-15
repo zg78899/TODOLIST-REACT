cba5f55f942909e18a3587b40296dd71b1f0779c
kim jae hun <zg788990@gmail.com>
2020-12-15 15:38:20 +0900

    revise

--- todolist/src/components/Footer/Footer.jsx
+++ todolist/src/components/Footer/Footer.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Footer=(props) => {
+const Footer = (props) => {
+  const { todos } = props;
+  const itemLeft = todos.filter((todo) => !todo.completed).length;
+  const countCompleted = todos.filter((todo) => todo.completed).length;
 
-  const {todos}=props;
-  const activeTodos=todos.filter(todo => !todo.completed).length;
-  const completedTodos=todos.filter(todo => todo.completed).length;
-
-    return (
-      <div className="footer">
-          <div className="complete-all">
-            <input className="custom-checkbox" type="checkbox" id="ck-complete-all" onChange={({ target }) => props.toggleAll(target.checked)} />
-            <label htmlFor="ck-complete-all">Mark all as complete</label>
-         </div>
-
-          <div className="clear-completed">
-            <button className="btn" onClick={props.removeAll}>Clear completed (<span className="completed-todos">{activeTodos}</span>)</button>
-            <strong className="active-todos">{completedTodos}</strong> items left
-          </div>
+  return (
+    <div className="footer">
+      <div className="complete-all">
+        <input
+          className="custom-checkbox"
+          type="checkbox"
+          id="ck-complete-all"
+          onChange={({ target }) => props.toggleAll(target.checked)}
+        />
+        <label htmlFor="ck-complete-all">Mark all as complete</label>
       </div>
-    );
-  }
 
+      <div className="clear-completed">
+        <button className="btn" onClick={props.removeAll}>
+          Clear completed (
+          <span className="completed-todos">{countCompleted}</span>)
+        </button>
+        <strong className="active-todos">{itemLeft}</strong> items left
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
